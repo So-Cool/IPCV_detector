79dbef951abbd844491bb1bf23414d26b7710a03
kacper <ks1591@bristol.ac.uk>
2013-11-14 15:26:07 +0000

    splitted into files

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
-all:
-	g++ face.cpp -o face.a $(O_LIBS)
+CC=g++
+MODULES=face.o filters.o
+CFLAGS=
+all: faces
+
+faces: face.o filters.o
+	$(CC) $(CFLAGS) $(MODULES) -o face.a $(O_LIBS)
+
+face.o: face.cpp
+	$(CC) -c face.cpp
+
+filters.o: filters.cpp
+	$(CC) -c filters.cpp
 
 run:
 	./face.a
---
+++ filters.hpp
@@ -0,0 +1,40 @@
+#ifndef FILTERS_HPP
+#define FILTERS_HPP
+
+#include "opencv.hpp"
+
+#define IMGTHRESHOLD 50
+#define HOUGHDETECTTRESHOLD 70
+
+//line
+//5 degrees is 0,087
+#define DTH 9 //delta angle * 100
+#define LINETHRESHOLD 220 //235
+#define LINEYDIM 314
+
+//circle
+#define RMIN 5
+#define RMAX 150//75
+#define CIRCLETHRESHOLD 210//220
+
+#define HOMOGENITYTHRS 40
+#define HOMOGENITYCOND 60  // more than 80% in confidence interval +- 20 in %'s'
+
+#define SHOW 0
+
+void sharpen(cv::Mat &input, int size, cv::Mat &out);
+
+void mexHat(cv::Mat &input, cv::Mat &out);
+//line detect
+void sobel(const cv::Mat& image, cv::Mat& xDeriv, cv::Mat& yDeriv, cv::Mat& grad, cv::Mat& arc);
+
+void detectLines(const cv::Mat& grad, const cv::Mat& arc, cv::Mat& out);
+
+void detectCircles(const cv::Mat& grad, const cv::Mat& arc, cv::Mat& out);
+
+//extract square region with given top left corner and side length
+void extractRegion(const cv::Mat& input, cv::Mat& output, int x, int y, int a);
+
+bool checkHomogenity(const cv::Mat& input);
+
+#endif
